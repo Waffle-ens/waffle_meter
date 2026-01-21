b356c8e82b7f0af436760d09acb0a895bbca31ae
TK-open-public <itaebread2@gmail.com>
2026-01-22 00:30:52 +0900

    Modify: battleTime 데이터 추가

--- src/main/kotlin/DpsCalculator.kt
+++ src/main/kotlin/DpsCalculator.kt
@@ -906,6 +906,7 @@ class DpsCalculator(private val dataStorage: DataStorage) {
                 data.damageContribution = data.amount / totalDamage * 100
             }
         }
+        dpsData.battleTime = battleTime
         return dpsData
     }
 
