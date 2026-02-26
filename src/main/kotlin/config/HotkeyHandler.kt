c1674ad56e8f9eb933230688d26f747ebf4cab17
TK-open-public <itaebread2@gmail.com>
2026-02-27 02:37:07 +0900

    단축키 관련 메소드 작성

--- src/main/kotlin/config/HotkeyHandler.kt
+++ src/main/kotlin/config/HotkeyHandler.kt
@@ -4,6 +4,7 @@ import com.sun.jna.platform.win32.User32
 import com.sun.jna.platform.win32.WinDef
 import com.sun.jna.platform.win32.WinUser
 import com.tbread.packet.PropertyHandler
+import com.tbread.webview.BrowserApp
 import org.slf4j.LoggerFactory
 
 object HotkeyHandler {
@@ -14,7 +15,11 @@ object HotkeyHandler {
 
     @Volatile
     private var currentHotkey: HotkeyCombo = loadHotkeyFromProperties()
-    
+
+    private var listenerThread: Thread? = null
+
+    @Volatile
+    private var running = false
 
     data class HotkeyCombo(val modifiers: Int, val vkCode: Int) {
         override fun toString() = "modifiers=$modifiers,vkCode=$vkCode"
@@ -40,4 +45,68 @@ object HotkeyHandler {
         return if (raw != null) HotkeyCombo.fromString(raw) ?: defaultHotkey else defaultHotkey
     }
 
+    fun getCurrentHotkey(): HotkeyCombo = currentHotkey
+
+
+    fun updateHotkey(modifiers: Int, vkCode: Int) {
+        currentHotkey = HotkeyCombo(modifiers, vkCode)
+        PropertyHandler.setProperty(propertyKey, currentHotkey.toString())
+        logger.info("단축키 변경: $currentHotkey")
+
+
+        stop()
+        start()
+    }
+
+    fun start() {
+        if (running) return
+        running = true
+        listenerThread = Thread({
+            val user32 = User32.INSTANCE
+            val hotkeyId = 1
+
+            val registered = user32.RegisterHotKey(
+                WinDef.HWND(null),
+                hotkeyId,
+                currentHotkey.modifiers,
+                currentHotkey.vkCode
+            )
+
+            if (!registered) {
+                logger.error("단축키 등록 실패: $currentHotkey")
+                running = false
+                return@Thread
+            }
+
+            logger.info("단축키 등록 성공: $currentHotkey")
+
+            val msg = WinUser.MSG()
+            while (running) {
+                if (user32.PeekMessage(msg, null, 0, 0, 1)) {
+                    if (msg.message == WinUser.WM_HOTKEY) {
+                        onHotkeyPressed()
+                    }
+                } else {
+                    Thread.sleep(10)
+                }
+            }
+
+            user32.UnregisterHotKey(null, hotkeyId)
+            logger.info("단축키 해제")
+        }, "HotkeyListener").apply { isDaemon = true }
+
+        listenerThread!!.start()
+    }
+
+    fun stop() {
+        running = false
+        listenerThread?.interrupt()
+        listenerThread = null
+    }
+
+
+    private fun onHotkeyPressed() {
+        logger.info("단축키 감지")
+        
+    }
 }
